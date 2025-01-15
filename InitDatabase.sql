7b8891b3e624fa00e089dad2ed7c010b6b732837
Ceartyme <csndz1505@gmail.com>
2025-01-15 13:27:25 +0100

    feat(InitDatabase.sql) : création de la base de données et remplissage items

--- InitDatabase.sql
+++ InitDatabase.sql
@@ -2,6 +2,16 @@ CREATE DATABASE IF NOT EXISTS IStore;
 
 USE IStore;
 
+DROP TABLE IF EXISTS WORKING;
+DROP TABLE IF EXISTS Inventories;
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Stores;
+DROP TABLE IF EXISTS Employees;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Roles;
+DROP TABLE IF EXISTS WhitelistedEmail;
+
+
 CREATE TABLE WhitelistedEmail(
     Email VARCHAR(50) PRIMARY KEY
 );
@@ -38,6 +48,31 @@ CREATE TABLE Stores(
 CREATE TABLE Items(
     IdItem INT AUTO_INCREMENT,
     Name VARCHAR(50) UNIQUE NOT NULL,
-    Price DOUBLE NOT NULL
+    Price DOUBLE NOT NULL,
+    PRIMARY KEY (IdItem),
+    CONSTRAINT check_price CHECK ( Price>=0 )
+);
 
-)
+CREATE TABLE Inventories(
+    IdStore int,
+    IdItem int,
+    Amount int default 1,
+    PRIMARY KEY (IdStore,IdItem),
+    FOREIGN KEY (IdStore) REFERENCES Stores(IdStore),
+    FOREIGN KEY (IdItem) REFERENCES Items(IdItem),
+    CONSTRAINT check_amount CHECK ( Amount>=0 )
+);
+
+CREATE TABLE WORKING(
+    IdStore int,
+    IdUser int,
+    PRIMARY KEY (IdStore,IdUser),
+    FOREIGN KEY (IdStore) REFERENCES Stores(IdStore),
+    FOREIGN KEY (IdUser) REFERENCES Users(IdUser)
+);
+
+INSERT INTO Items (Name, Price) VALUES ('Laptop', 999.99);
+INSERT INTO Items (Name, Price) VALUES ('Smartphone', 699.99);
+INSERT INTO Items (Name, Price) VALUES ('Headphones', 149.99);
+INSERT INTO Items (Name, Price) VALUES ('Keyboard', 49.99);
+INSERT INTO Items (Name, Price) VALUES ('Mouse', 29.99);
