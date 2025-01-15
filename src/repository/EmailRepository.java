203264cc6afbe9a9756f0c998618a4c885b9e43c
Ceartyme <csndz1505@gmail.com>
2025-01-15 23:31:23 +0100

    feat(emailRepository) : correction du isEmailWhitelisted (il manquait la ligne rs.next ce qui causait une erreur

--- src/repository/EmailRepository.java
+++ src/repository/EmailRepository.java
@@ -12,6 +12,7 @@ public class EmailRepository {
             PreparedStatement pstmt = conn.prepareStatement("SELECT COUNT(*) FROM WhitelistedEmail WHERE Email=?");
             pstmt.setString(1, email);
             ResultSet rs = pstmt.executeQuery();
+            rs.next();
             return new Response<>(rs.getInt(1)==1);
         } catch (SQLException e) {
             return new Response<>("SQL Error : "+e.getMessage());
