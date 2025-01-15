a3da6fd4d3949bbe35ad6bd507378c3c06ee5f13
Ceartyme <csndz1505@gmail.com>
2025-01-15 23:33:28 +0100

    feat(UserRepository) : ajout de  CreateUser et de login, installation des librairies common logging et spring security pour l'encodage du password

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,33 +1,76 @@
 package repository;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import model.Response;
 import model.User;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class UserRepository {
     public static Response<ArrayList<User>> getAllUsers(){
         Connection conn = null;
+
         ArrayList<User> users = new ArrayList<>();
         try {
             conn = Repository.getConnection();
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM Users;");
-            if(rs==null){
+            if(!rs.next()){
                 return new Response<>("Error not any user in database");
             }
-            while(rs.next()){
+            do{
                 User t = new User(rs.getInt("IdUser"),rs.getString("Email"),rs.getString("Password"),rs.getString("Username"),rs.getInt("IdRole"));
                 users.add(t);
-            }
+            }while(rs.next());
             return new Response<>(users);
         } catch (SQLException e) {
             return new Response<>("SQL Error : "+e.getMessage());
         }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static String createUser(String username,String email, String password, int idRole){
+        Connection conn =null;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode(password);
+        try{
+            conn = Repository.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Users(Email, Password, Username, IdRole) VALUE(?,?,?,?)");
+            pstmt.setString(1,email);
+            pstmt.setString(2,hashedPassword);
+            pstmt.setString(3,username);
+            pstmt.setInt(4,idRole);
+            pstmt.executeUpdate();
+            return "User Created Successfully";
+        }catch(SQLException e){
+            return "SQL ERROR : "+e.getMessage();
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static Response<User> login(String email, String password) {
+        Connection conn = null;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        try {
+            conn = Repository.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM Users WHERE Email=?");
+            pstmt.setString(1, email);
+            ResultSet rs = pstmt.executeQuery();
+            if (!rs.next()) {
+                return new Response<>("Incorrect Email or Password");
+            }
+            if (!encoder.matches(password, rs.getString("Password"))) {
+                return new Response<>("Incorrect Email or Password");
+            }else{
+                return new Response<>(new User(rs.getInt("IdUser"),rs.getString("Email"),rs.getString("Password"),rs.getString("Username"),rs.getInt("IdRole")));
+            }
+        } catch (SQLException e) {
+            return new Response<>("SQL ERROR : " + e.getMessage());
+        }finally{
             Repository.closeConnection(conn);
         }
     }
