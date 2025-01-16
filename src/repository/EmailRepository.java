5f95915de294a29cbe998d9b166732fdda1a0694
Ceartyme <csndz1505@gmail.com>
2025-01-16 21:42:32 +0100

    feat(EmailRepository) : ajout de GetAllEmail et whitelistEmail

--- src/repository/EmailRepository.java
+++ src/repository/EmailRepository.java
@@ -3,6 +3,7 @@ package repository;
 import model.Response;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class EmailRepository {
     public static Response<Boolean> isEmailWhitelisted(String email){
@@ -16,6 +17,42 @@ public class EmailRepository {
             return new Response<>(rs.getInt(1)==1);
         } catch (SQLException e) {
             return new Response<>("SQL Error : "+e.getMessage());
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static Response<ArrayList<String>> getAllEmail(){
+        Connection conn = null;
+        ArrayList<String> emails = new ArrayList<>();
+        try{
+            conn = Repository.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM WhitelistedEmail;");
+            if(!rs.next()){
+                return new Response<>("There are no whitelisted email");
+            }
+            do{
+                emails.add(rs.getString("Email"));
+            }while (rs.next());
+            return new Response<>(emails);
+        }catch (SQLException e){
+            return new Response<>("SQL ERROR : "+e.getMessage());
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static String whitelistEmail(String email){
+        Connection conn = null;
+        try{
+            conn = Repository.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO WhitelistedEmail(Email) VALUE(?)");
+            pstmt.setString(1,email);
+            pstmt.executeUpdate();
+            return "Email whitelisted Successfully";
+        }catch (SQLException e){
+            return "SQL ERROR : "+e.getMessage();
         }finally {
             Repository.closeConnection(conn);
         }
