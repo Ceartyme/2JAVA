46847f4aec5a9d0d160fe0136c916c531d1dee1b
Ceartyme <csndz1505@gmail.com>
2025-01-17 14:04:00 +0100

    feat(ItemRepository) : ajout de CreateItem, deleteItem, getItemById,getStores

--- src/repository/ItemRepository.java
+++ src/repository/ItemRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import model.Inventory;
 import model.Item;
 import model.Response;
 
@@ -14,7 +15,7 @@ public class ItemRepository {
             conn = Repository.getConnection();
             Statement stmt = conn.createStatement();
             stmt.executeQuery("USE IStore;");
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Items");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Items;");
             if(!rs.next()){
                 return new Response<>("There are no Items in the database");
             }
@@ -22,6 +23,79 @@ public class ItemRepository {
                 items.add(new Item(rs.getInt("IdItem"),rs.getString("Name"),rs.getDouble("Price")));
             }while (rs.next());
             return new Response<>(items);
+        }catch (SQLException e){
+            return new Response<>("SQL ERROR : "+e.getMessage());
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static String createItem(String name, double price){
+        Connection conn = null;
+        try{
+            conn = Repository.getConnection();
+            conn.createStatement().executeQuery("USE IStore;");
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Items(Name, Price) VALUE (?,?);");
+            pstmt.setString(1,name);
+            pstmt.setDouble(2,price);
+            pstmt.executeUpdate();
+            return "Item created Successfully";
+        }catch (SQLException e){
+            return "SQL ERROR : "+e.getMessage();
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static String deleteItem(int id){
+        Connection conn = null;
+        try{
+            conn = Repository.getConnection();
+            conn.createStatement().executeQuery("USE IStore;");
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM Items WHERE IdItem=?;");
+            pstmt.setInt(1,id);
+            pstmt.executeUpdate();
+            return "Item deleted Successfully";
+        }catch (SQLException e){
+            return "SQL ERROR : "+e.getMessage();
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static Response<Item> getItemById(int id){
+        Connection conn = null;
+        try {
+            conn = Repository.getConnection();
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery("USE IStore;");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Items WHERE IdItem="+id+";");
+            if(!rs.next()){
+                return new Response<>("No item with that id in the database");
+            }
+            return new Response<>(new Item(rs.getInt("IdItem"),rs.getString("Name"),rs.getDouble("Price")));
+        }catch (SQLException e){
+            return new Response<>("SQL ERROR : "+e.getMessage());
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static Response<ArrayList<Inventory>> getStores(int id){
+        Connection conn = null;
+        ArrayList<Inventory> inventories = new ArrayList<>();
+        try{
+            conn = Repository.getConnection();
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery("USE IStore;");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Inventories WHERE IdItem="+id+";");
+            if(!rs.next()){
+                return new Response<>("there are no item with that id in any store");
+            }
+            do{
+                inventories.add(new Inventory(rs.getInt("IdStore"),rs.getInt("IdItem"),rs.getInt("Amount")));
+            }while (rs.next());
+            return new Response<>(inventories);
         }catch (SQLException e){
             return new Response<>("SQL ERROR : "+e.getMessage());
         }finally {
