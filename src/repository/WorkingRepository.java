d441e26b13eb2adcf2d32a88ea6144fc0ce6c518
Ceartyme <csndz1505@gmail.com>
2025-01-17 17:59:01 +0100

    feat(WorkingRepository) : ajout de getEmployeesFromStore

--- src/repository/WorkingRepository.java
+++ src/repository/WorkingRepository.java
@@ -1,8 +1,14 @@
 package repository;
 
+import model.Response;
+import model.User;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class WorkingRepository {
     public static String hire(int idStore, int idUser){
@@ -31,6 +37,30 @@ public class WorkingRepository {
             return "Employee fired Successfully";
         }catch (SQLException e){
             return "SQL ERROR : "+e.getMessage();
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+
+    public static Response<ArrayList<User>> getEmployeesFromStore(int idStore){
+        Connection conn = null;
+        ArrayList<User> employees = new ArrayList<>();
+        try {
+            conn = Repository.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM IStore.WORKING WHERE IdStore="+idStore+";");
+            if(!rs.next()){
+                return new Response<>("There are no employees in this store");
+            }
+            do {
+                Response<User> userResponse = UserRepository.getUserById(rs.getInt("IdUser"));
+                if(Objects.equals(userResponse.getMessage(), "Success")) {
+                    employees.add(userResponse.getValue());
+                }
+            }while (rs.next());
+            return new Response<>(employees);
+        }catch (SQLException e){
+            return new Response<>("SQL ERROR : "+e.getMessage());
         }finally {
             Repository.closeConnection(conn);
         }
