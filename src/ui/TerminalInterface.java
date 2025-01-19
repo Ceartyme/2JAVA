5e997b2f242e8b0626ad6c0ebaea0be1730c7fd7
Ceartyme <csndz1505@gmail.com>
2025-01-19 16:28:12 +0100

    feat(TerminalInterface) : ajout de la fonction du choix d'interface

---
+++ src/ui/TerminalInterface.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class TerminalInterface {
+    public static void AskChoiceInterface(){
+        System.out.println("╔═══════════════════════════════════════════╗");
+        System.out.println("║       Which interface do you want ?       ║");
+        System.out.println("║                                           ║");
+        System.out.println("║          1. Terminal                      ║");
+        System.out.println("║          2. Swing Window                  ║");
+        System.out.println("║                                           ║");
+        System.out.println("╚═══════════════════════════════════════════╝");
+    }
+}
