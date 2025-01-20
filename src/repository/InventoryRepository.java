df67b7b232befd4522f5c4f7697b2ecac4fb0046
Ceartyme <csndz1505@gmail.com>
2025-01-20 12:21:12 +0100

    feat(InventoryRepository) : ajout du getItemsByStore

---
+++ src/repository/InventoryRepository.java
@@ -0,0 +1,33 @@
+package repository;
+
+import model.Inventory;
+import model.Response;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class InventoryRepository {
+    public static Response<ArrayList<Inventory>> getItemsByStore(int idStore){
+        Connection conn = null;
+        ArrayList<Inventory> inventories = new ArrayList<>();
+        try{
+            conn = Repository.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM IStore.Inventories WHERE IdStore="+idStore+";");
+            if(!rs.next()){
+                return new Response<>("This store does not exist or does not have any employee");
+            }
+            do {
+                inventories.add(new Inventory(idStore,rs.getInt("Amount"),rs.getInt("IdItem")));
+            }while (rs.next());
+            return new Response<>(inventories);
+        }catch (SQLException e){
+            return new Response<>("SQL ERROR : "+e.getMessage());
+        }finally {
+            Repository.closeConnection(conn);
+        }
+    }
+}
