faf11ed64351321db13ee8820a2a0174bbdd049f
Ceartyme <csndz1505@gmail.com>
2025-01-21 23:10:22 +0100

    feat(InputService) : ajout d'une fonction pour rentrer une adresse mail

--- src/service/InputService.java
+++ src/service/InputService.java
@@ -2,6 +2,7 @@ package service;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputService{
     public static int intInput(int minimum, int maximum, Scanner input){
@@ -17,5 +18,19 @@ public class InputService{
             input.nextLine();
             return intInput(minimum, maximum, input);
         }
+    }
+
+    public static String emailInput(Scanner input){
+        System.out.println("Enter a valid email address : ");
+        String email=input.next();
+        if (!isEmailValid(email)){
+            emailInput(input);
+        }
+        return email;
+    }
+
+    public static boolean isEmailValid(String email){
+        String emailRegex = "^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,3}$";
+        return Pattern.compile(emailRegex).matcher(email).matches();
     }
 }
